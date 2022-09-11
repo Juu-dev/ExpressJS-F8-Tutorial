c85107396f118662fc2b1bcdab0f9fe258bbc95a
Juu-dev <Thong.pm772002@gmail.com>
2022-09-11 17:16:53 +0700

    Express handlebar and static file

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// I'm loading the file by ECMAScript module which does'n have __dirname or __filename
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// import express (framwork base on Node.js)
+import express from "express";
+// import morgan (HTTP request logger middleware for Node.js )
+import morgan from "morgan";
+// import express-handlebars (template code)
+import { engine } from "express-handlebars";
+
+const app = express();
+const port = 3000;
+
+console.log(__dirname);
+
+// static file => access path: "__dirname/public"
+app.use(express.static(path.join(__dirname, "public")));
+
+//HTTP logger
+app.use(morgan("combined"));
+
+// Template Engine
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
