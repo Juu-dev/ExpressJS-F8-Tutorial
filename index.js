0eb77e1feea02adc83cb43b912a8b7f9c680cb6e
Juu-dev <Thong.pm772002@gmail.com>
2022-09-11 13:45:45 +0700

    Install nodemon and Morgan

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var morgan = require("morgan");
+
+var app = express();
+
+app.use(morgan("combined"));
 
 app.get("/", function (req, res) {
   res.send("Hello World");
